d056e79fe9ece3b10ccfc7e379b28b328cbc1c88
Yaroslav <yaroslav@wannabe.pro>
2019-01-14 20:55:37 +0500

    Typo fix

--- payment/qiwikassa_checkout/template/lang/ru/template.php
+++ payment/qiwikassa_checkout/template/lang/ru/template.php
@@ -2,6 +2,6 @@
 
 $MESS['SALE_HANDLERS_QIWI_KASSA_DESCRIPTION'] = 'Услугу предоставляет сервис онлайн-платежей <b>&laquo;Qiwi.Кассы&raquo;</b>.<br /><br />Сумма к оплате по счету: ';
 $MESS['SALE_HANDLERS_QIWI_KASSA_WARNING_RETURN'] = '<b>Обратите внимание:</b> если вы откажетесь от покупки, для возврата денег вам придется обратиться в магазин.';
-$MESS['SALE_HANDLERS_QIWI_KASSA_REDIRECT'] = 'вы будете перенаправленны на страницу оплаты';
+$MESS['SALE_HANDLERS_QIWI_KASSA_REDIRECT'] = 'вы будете перенаправлены на страницу оплаты';
 $MESS['SALE_HANDLERS_QIWI_KASSA_BUTTON_PAID'] = 'Оплатить';
-$MESS['SALE_HANDLERS_QIWI_KASSA_REDIRECT_MESS'] = 'Вы будете перенаправленны на страницу оплаты';
+$MESS['SALE_HANDLERS_QIWI_KASSA_REDIRECT_MESS'] = 'Вы будете перенаправлены на страницу оплаты';
