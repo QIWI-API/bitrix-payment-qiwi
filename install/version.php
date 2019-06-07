1f2cb1af04a9d9892d913305514def3291f5ac6b
Yaroslav <yaroslav@wannabe.pro>
2019-06-07 19:23:42 +0500

    Version 0.0.3 * Add bill live time setting * Add popup payment form * Add debug mode * Remove bill prefix * Remove pay status ignore setting

--- install/version.php
+++ install/version.php
@@ -2,6 +2,6 @@
 
 /** @var array $arModuleVersion Module version metadata. */
 $arModuleVersion = [
-    'VERSION'      => '0.0.2',
+    'VERSION'      => '0.0.3',
     'VERSION_DATE' => '2019-02-14 00:00',
 ];
